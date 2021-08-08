d2f484646217e8dd658166b855c5be051de7c2f4
Hussein Hafez <husseinhafez690@gmail.com>
2021-08-08 20:34:06 +0200

    .

---
+++ Pose Estimation/main.py
@@ -0,0 +1,31 @@
+import cv2
+import mediapipe as mp
+import time
+
+
+mpDraw = mp.solutions.drawing_utils()
+mpPose = mp.solutions.pose
+pose = mpPose.Pose()
+
+cap  = cv2.VideoCapture(0)
+pTime = 0
+
+while True:
+    _, frame = cap.read()
+    imgRGB = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+    results = pose.process(imgRGB)
+    if results.pose_landmarks:
+        mpDraw.draw_landmarks(frame, results.pose_landmarks, mpPose.POSE_CONNECTIONS)
+
+    cTime = time.time()
+    fps = 1/(cTime-pTime)
+    pTime = cTime
+
+    cv2.putText(frame, str(int(fps)), (70,50), cv2.FONT_HERSHEY_PLAIN, 3, (255,0,0), 3)
+
+    cv2.imshow("frame", frame)
+
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
+
+cv2.destroyAllWindows()

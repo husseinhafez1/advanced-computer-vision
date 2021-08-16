a13d2be0db1ae104803edd575ddbe5e7cfb93fcd
husseinhafez1 <husseinhafez690@gmail.com>
2021-08-17 01:40:39 +0200

    added the pose estimation module

---
+++ Pose Estimation/PoseEstimationModule.py
@@ -0,0 +1,36 @@
+import cv2
+import mediapipe as mp
+
+
+class PoseEstimation():
+    def __init__(self, mode=False, upBody=False, smooth=False, detectionCon=0.5, trackCon=0.5):
+        self.mode = mode
+        self.upBody = upBody
+        self.smooth = smooth
+        self.detectionCon = detectionCon
+        self.trackCon = trackCon
+
+        self.mpPose = mp.solutions.pose
+        self.pose = self.mpPose.Pose(self.mode, self.upBody, self.smooth, self.detectionCon, self.trackCon)
+        self.mpDraw = mp.solutions.drawing_utils
+
+    def findPose(self, frame, draw=True):
+        imgRgb = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+        self.results = self.pose.process(imgRgb)
+
+        if self.results.pose_landmarks:
+            if draw:
+                self.mpDraw.draw_landmarks(frame, self.results.pose_landmarks, self.mpPose.POSE_CONNECTIONS)
+        return frame
+    
+    def findPosition(self, frame, draw=True):
+        lmList = []
+        if self.results.pose_landmarks:
+            for id, lm in enumerate(self.results.pose_landmarks.landmark):
+                h, w, c = frame.shape
+                cx, cy = int(lm.x*w), int(lm.y*h)
+                lmList.append([id, cx, cy])
+                if draw:
+                    cv2.circle(frame, (cx,cy), 15, (255,0,255), cv2.FILLED)
+
+        return lmList
---
+++ Pose Estimation/poseGameTracking.py
@@ -0,0 +1,30 @@
+import cv2
+import time
+import mediapipe as mp
+import PoseEstimationModule
+from PoseEstimationModule import PoseEstimation
+
+
+cap = cv2.VideoCapture(0)
+pTime = 0
+cTime = 0
+detector = PoseEstimation()
+while True:
+    _, frame = cap.read()
+    frame = detector.findPose(frame)
+    lmList = detector.findPosition(frame, draw=False)
+    if len(lmList) != 0:
+        print(lmList[4])
+
+    cTime = time.time()
+    fps = 1/(cTime-pTime)
+    pTime = cTime
+
+    cv2.putText(frame, str(int(fps)), (10,70), cv2.FONT_HERSHEY_PLAIN, 3, (255,0,0), 3)
+
+    cv2.imshow("frame", frame)
+    if cv2.waitKey(1) & 0xFF == ord("q"):
+        break
+
+
+cv2.destroyAllWindows()
